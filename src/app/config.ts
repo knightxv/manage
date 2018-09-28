be08104cd51e3e5a0ca87bf6dba05bc68d6875dd
knightxv <962068006@qq.com>
2018-09-28 18:48:54 +0800

    test delete all

--- src/app/config.ts
+++
@@ -1,5 +0,0 @@
-// http://21b97p5647.imwork.net:32799
-const IS_DEBUG = process.env.NODE_ENV === 'development';
-export const REMOTE_URL = IS_DEBUG ? 'http://192.168.1.10:8002' : 'http://21b97p5647.imwork.net/api/manage';
-export const BASE_URL = 'manage';
-export default module.exports;
--- src/stores/action-types.ts
+++
@@ -1,4 +0,0 @@
-// login
-export const LOGOUT = 'LOGOUT';
-// user
-export const UPDATE_USER_INFO_ASYNC = 'UPDATE_USER_INFO_ASYNC';
